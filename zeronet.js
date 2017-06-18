64080eb34d32744393956fcedb2292ce17de0cc6
mkg20001 <mkg20001@gmail.com>
2017-06-18 15:49:49 +0200

    Add still broken ssl/tls stuff

--- zeronet.js
+++ zeronet.js
@@ -1,6 +1,9 @@
 const ZeroNet = require("./lib/zeronet")
 const Client = require("./lib/client/index.js")
 
+const fs = require("fs")
+const read = file => fs.readFileSync(file)
+
 const zeronet = new ZeroNet({
   server: {
     host: "0.0.0.0",
@@ -12,6 +15,10 @@ const c = new Client({
   target: {
     host: "localhost",
     port: 15542
+  },
+  tls: {
+    cert: read("./cert.pem"),
+    key: read("./key.pem")
   }
 }, zeronet)
 c.getFile("1HeLLo4uzjaLetFx6NH3PMwFP3qbRbTf3D", "content.json", 1, console.log)
