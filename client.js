c557afde9f003c4d8c344d21032497c16c20a7e6
mkg20001 <mkg20001@gmail.com>
2017-06-18 12:34:26 +0200

    Add more code - Make connections easier

--- client.js
+++ client.js
@@ -1,14 +1,8 @@
-const net = require('net')
-
-const msgpack = require("msgpack")
-
-const Protocol = require("./lib/protocol/")
-
-const sock = net.connect({
-  host: "localhost",
-  port: 15441
-}, err => {
-  if (err) console.error(err)
-  const client = new Protocol(sock)
-  client.getFile("1HeLLo4uzjaLetFx6NH3PMwFP3qbRbTf3D", "content.json", 100, console.log)
+const client = require("./lib/client/index.js")
+const c = new client({
+  target: {
+    host: "localhost",
+    port: "15542"
+  }
 })
+c.getFile("1HeLLo4uzjaLetFx6NH3PMwFP3qbRbTf3D", "content.json", 100, console.log)
