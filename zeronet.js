e31040751cae1863d0b11b87b37475184322ae0b
mkg20001 <mkg20001@gmail.com>
2017-06-18 16:37:08 +0200

    Add better logging (bunyan) - Work on tls - Other things

--- zeronet.js
+++ zeronet.js
@@ -7,19 +7,18 @@ const read = file => fs.readFileSync(file)
 const zeronet = new ZeroNet({
   server: {
     host: "0.0.0.0",
-    port: 15542
+    port: 15543
   }
 })
 
 const c = new Client({
   target: {
     host: "localhost",
-    port: 15542
+    port: 15543
   },
   tls: {
     cert: read("./cert.pem"),
     key: read("./key.pem")
   }
 }, zeronet)
-//
 c.handshake(() => c.getFile("1HeLLo4uzjaLetFx6NH3PMwFP3qbRbTf3D", "content.json", 1, console.log))
