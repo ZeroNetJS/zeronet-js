d6aa31b291472c4e650d98b0581b9e7834d6cca4
mkg20001 <mkg20001@gmail.com>
2017-06-22 14:41:08 +0200

    Fix handshake test

--- test/tls/tls-handshake.js
+++ test/tls/tls-handshake.js
@@ -5,7 +5,8 @@ it("should connect via tls handshake", (cb) => {
       port: 15543
     }
   }, zeronet)
-  c.handshake((err) => {
+  c.handshake((err, handshake) => {
+    if (!handshake.commonCrypto()) return cb(new Error("Test: No tls was used"))
     if (err) return cb(err)
     else c.getFile("1HeLLo4uzjaLetFx6NH3PMwFP3qbRbTf3D", "content.json", 1, err => {
       if (err) console.error("Error, but not failing the test", err)
