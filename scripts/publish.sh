626f497beaefec78c0f0e391a535218cd66aab73
mkg20001 <mkg20001@gmail.com>
2017-06-25 15:20:24 +0200

    Fix some bugs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,16 +14,23 @@ done
 
 files="$files . "
 
-echo "Publishing $files @ $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g")"
+ver=$(echo $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g" | grep " .*" -o))
+
+echo "Publishing $files @ $ver"
 
 echo "Sure?"
+
+sed -r 's|"([a-z-]+)": "file:.*"|"\1": "'$ver'"|g' -i package.json
 
 read foo
 
 [ "$foo" != "yes" ] && echo "Abort." && exit 2
 
+set -x
+
 for dir in $files; do
   cd $dir
+    [ ! -e .gitignore ] && ln -s ../.gitignore
     npm publish
   cd ..
 done
