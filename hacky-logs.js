cd5b29de79770b6b1f40abae1d01918e1342c832
mkg20001 <mkg20001@gmail.com>
2017-07-18 12:52:23 +0200

    Seperate log-hacks from cli code

---
+++ hacky-logs.js
@@ -0,0 +1,115 @@
+"use strict"
+
+const mockery = require("mockery")
+
+let dwait = []
+
+const _debug = require("debug")
+
+mockery.enable({
+  warnOnReplace: false,
+  warnOnUnregistered: false
+})
+
+mockery.registerMock("debug", function createDebug(name) {
+  let que = []
+  let qq = false
+  let rd
+  if (process.env.DEBUG) {
+    rd = _debug(name)
+  }
+
+  function write(data) {
+    if (global.ZeroLogWS) global.ZeroLogWS.write(JSON.stringify(data) + "\n")
+    else que.push(data)
+  }
+
+  function processQue() {
+    if (!qq) {
+      dwait.push(processQue)
+      qq = true
+    }
+    que.forEach(d => write(d))
+    que = null
+  }
+
+  function debug() {
+    write({
+      debug: true,
+      time: new Date().getTime(),
+      name,
+      data: [...arguments]
+    })
+    if (rd) rd.apply(rd, arguments)
+  }
+
+  return debug
+})
+
+function consoleMock(type) {
+  let que = []
+  let qq = false
+  let rd
+  const o = console[type].bind(console)
+
+  function write(data) {
+    if (global.ZeroLogWS) global.ZeroLogWS.write(JSON.stringify(data) + "\n")
+    else que.push(data)
+  }
+
+  function processQue() {
+    if (!qq) {
+      dwait.push(processQue)
+      qq = true
+    }
+    que.forEach(d => write(d))
+    que = null
+  }
+
+  const d = {
+    "bound consoleCall": function () {
+      write({
+        console: true,
+        time: new Date().getTime(),
+        type,
+        data: [...arguments]
+      })
+      o.apply(o, arguments)
+    }
+  }
+
+  return d["bound consoleCall"]
+}
+
+Object.keys(console).forEach(key => {
+  console[key] = consoleMock(key)
+})
+
+const bunyan = require("./bunyan") //we have patched that one
+
+const Transform = require("stream").Transform
+
+const through = new Transform({
+  transform(data, enc, cb) {
+    this.push(data)
+    cb()
+  }
+})
+
+const fakeProcess = { //the whole object is a lie
+  env: process.env,
+  versions: process.versions,
+  platform: process.platform,
+  stdin: through,
+  stdout: process.stderr,
+  stderr: process.stderr,
+  on: () => {},
+  //exit: code => exit(code)
+}
+
+bunyan(fakeProcess)
+
+delete process.stdout
+process.stdout = through
+
+module.exports = dwait
--- zeronet.js
+++ zeronet.js
@@ -1,123 +1,13 @@
 "use strict"
 
-const mockery = require("mockery")
-
 let node
-let dwait = []
-
-const _debug = require("debug")
-
-mockery.enable({
-  warnOnReplace: false,
-  warnOnUnregistered: false
-})
-
-mockery.registerMock("debug", function createDebug(name) {
-  let que = []
-  let qq = false
-  let rd
-  if (process.env.DEBUG) {
-    rd = _debug(name)
-  }
-
-  function write(data) {
-    if (global.ZeroLogWS) global.ZeroLogWS.write(JSON.stringify(data) + "\n")
-    else que.push(data)
-  }
-
-  function processQue() {
-    if (!qq) {
-      dwait.push(processQue)
-      qq = true
-    }
-    que.forEach(d => write(d))
-    que = null
-  }
-
-  function debug() {
-    write({
-      debug: true,
-      time: new Date().getTime(),
-      name,
-      data: [...arguments]
-    })
-    if (rd) rd.apply(rd, arguments)
-  }
-
-  return debug
-})
-
-function consoleMock(type) {
-  let que = []
-  let qq = false
-  let rd
-  const o = console[type].bind(console)
-
-  function write(data) {
-    if (global.ZeroLogWS) global.ZeroLogWS.write(JSON.stringify(data) + "\n")
-    else que.push(data)
-  }
-
-  function processQue() {
-    if (!qq) {
-      dwait.push(processQue)
-      qq = true
-    }
-    que.forEach(d => write(d))
-    que = null
-  }
-
-  const d = {
-    "bound consoleCall": function () {
-      write({
-        console: true,
-        time: new Date().getTime(),
-        type,
-        data: [...arguments]
-      })
-      o.apply(o, arguments)
-    }
-  }
-
-  return d["bound consoleCall"]
-}
-
-Object.keys(console).forEach(key => {
-  console[key] = consoleMock(key)
-})
+let dwait = require("./hacky-logs.js")
 
 const fs = require("fs")
 const path = require("path")
 
 const MergeRecursive = require("merge-recursive")
 const ZeroNet = require("zeronet-node")
-
-const bunyan = require("./bunyan") //we have patched that one
-
-const Transform = require("stream").Transform
-
-const through = new Transform({
-  transform(data, enc, cb) {
-    this.push(data)
-    cb()
-  }
-})
-
-const fakeProcess = { //the whole object is a lie
-  env: process.env,
-  versions: process.versions,
-  platform: process.platform,
-  stdin: through,
-  stdout: process.stderr,
-  stderr: process.stderr,
-  on: () => {},
-  //exit: code => exit(code)
-}
-
-bunyan(fakeProcess)
-
-delete process.stdout
-process.stdout = through
 
 const FS = require("zeronet-storage-fs")
 
