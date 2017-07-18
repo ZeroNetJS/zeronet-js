0072db7b671b4ae1e939c193e8f9e3f2fd2c3658
mkg20001 <mkg20001@gmail.com>
2017-07-18 14:20:26 +0200

    pkg fixes

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if node -v | grep "^v8"; then
+if node -v | grep "^v8" > /dev/null; then
 
-  set -ex
+  set -e
 
   rm -rf .pkg
   mkdir .pkg
-  for f in package.json zeronet.js; do cp $f .pkg; done
+  for f in package* *.js; do cp $f .pkg; done
   ver=$(echo $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g" | grep " .*" -o) | sed "s|,||g")
   cd .pkg
-  sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../\2.tar.gz"|g' -i package.json
+  for f in package*; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../\2.tar.gz"|g' -i $f; done
   npm i --production
   ../node_modules/.bin/pkg -t node8-linux-x64,node8-macos-x64,node8-win-x64 .
 
