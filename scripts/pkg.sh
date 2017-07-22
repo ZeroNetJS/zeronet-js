225d9b173f197e62148465086a9fdbb2762ef9c0
mkg20001 <mkg20001@gmail.com>
2017-07-22 11:03:15 +0200

    Cleanup more

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -6,7 +6,7 @@ if node -v | grep "^v8" > /dev/null; then
 
   rm -rf .pkg
   mkdir .pkg
-  for f in package* *.js; do cp $f .pkg; done
+  for f in package* zeronet.js lib; do cp -r $f .pkg; done
   ver=$(echo $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g" | grep " .*" -o) | sed "s|,||g")
   cd .pkg
   for f in package*; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../\2.tar.gz"|g' -i $f; done
