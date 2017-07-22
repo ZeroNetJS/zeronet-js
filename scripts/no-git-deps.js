2df33453e82c83c4d96db6eadb15f8b6789d98ea
mkg20001 <mkg20001@gmail.com>
2017-07-22 13:06:59 +0200

    Fix snap

---
+++ scripts/no-git-deps.js
@@ -0,0 +1,24 @@
+const file = process.argv[2]
+const fs = require("fs")
+const load = file => JSON.parse(fs.readFileSync(file).toString())
+const save = (c, f) => fs.writeFileSync(f, JSON.stringify(c, null, 2))
+let c = load(file)
+
+function del(o) {
+  if (o.version && o.version.startsWith("github")) return true
+  for (var p in o) {
+    if (typeof o[p] == "object") {
+      if (del(o[p])) {
+        console.log("del", o[p])
+        delete o[p]
+      }
+    } else if (typeof o[p] == "string" && o[p].startsWith("github")) {
+      console.log("del", o[p])
+      delete o[p]
+    }
+  }
+}
+
+del(c)
+
+save(c, file)
