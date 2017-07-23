352c09988a722f0b02cfac87630ec9e7c7fb3853
mkg20001 <mkg20001@gmail.com>
2017-07-23 11:46:42 +0200

    Fix snap?

--- scripts/global.sh
+++ scripts/global.sh
@@ -15,7 +15,7 @@ mkdir .pkg
 for f in package* zeronet.js lib npm-shrinkwrap.json .gitignore LICENSE; do cp -r $f .pkg; done
 ver=$(echo $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g" | grep " .*" -o) | sed "s|,||g")
 cd .pkg
-for f in package* npm-shrinkwrap.json; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../../\2.tar.gz"|g' -i $f; done
+for f in package* npm-shrinkwrap.json; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:$PWD/../\2.tar.gz"|g' -i $f; done
 ex_re
 tar cvfz ../znjs.tar.gz $ex --mode="777" . | sed "s|^|zeronet-js: |g"
 mv ../znjs.tar.gz .
