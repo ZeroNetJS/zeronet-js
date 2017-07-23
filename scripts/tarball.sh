26a26bc1b34004cbc9f3af55711db919cf9453bb
mkg20001 <mkg20001@gmail.com>
2017-07-23 11:58:15 +0200

    Fix snap?

--- scripts/tarball.sh
+++ scripts/tarball.sh
@@ -5,8 +5,7 @@ set -e
 op="$PWD"
 
 ex_re() {
-  ex="--exclude=zeronet-* --exclude=package-lock.json"
-  [ ! -z "$1" ] && other="$@" || other=$(cat $op/.gitignore)
+  [ ! -z "$1" ] && other="$@" || other=$(cat $op/.gitignore) && ex="--exclude=zeronet-* --exclude=package-lock.json"
   for f in .git .gitignore .travis.yml debug scripts .empty *.yml snap data $other; do
     ex="$ex --exclude=$f"
   done
@@ -15,17 +14,17 @@ ex_re() {
 if ([ "$0" = "$BASH_SOURCE" ] || ! [ -n "$BASH_SOURCE" ]);
 then
 
-ex_re
-tar cvfz zeronet.tar.gz $ex --mode="777" . | sed "s|^|zeronet: |g"
-ex_re
+  ex_re
+  tar cvfz zeronet.tar.gz $ex --mode="777" . | sed "s|^|zeronet: |g"
+  ex_re
 
-for file in $(dir -w 1 | grep "^zeronet-"); do
-  if [ -d $file ]; then
-    cd $file
-    tar cvfz ../$file.tar.gz $ex . | sed "s|^|$file: |g"
-    ex_re
-    cd ..
-  fi
-done
+  for file in $(dir -w 1 | grep "^zeronet-"); do
+    if [ -d $file ]; then
+      cd $file
+      tar cvfz ../$file.tar.gz $ex . | sed "s|^|$file: |g"
+      ex_re
+      cd ..
+    fi
+  done
 
 fi
