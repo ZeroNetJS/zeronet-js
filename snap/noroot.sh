8813c84a4f43db6a07891124501a4786e5c365c6
mkg20001 <mkg20001@gmail.com>
2017-07-23 09:34:21 +0200

    Fix snap

--- snap/noroot.sh
+++ snap/noroot.sh
@@ -1,3 +1,9 @@
 noroot() {
-  [ "$(id -u)" == "0" ] && (su $(cat /etc/passwd | grep -v "/bin/false" | grep "[a-z][a-z]*:*:[0-9][0-9][0-9][0-9]:" | sed -r "s|([a-z]+).+|\1|g") --shell /bin/bash "$1" ; exit $?)
+  if [ "$(id -u)" == "0" ]; then
+    echo "You are running as root???"
+    chmod 777 -R $PWD
+    chmod 777 -R $SNAPCRAFT_PART_INSTALL
+    su $(cat /etc/passwd | grep -v "/bin/false" | grep "[a-z][a-z]*:*:[0-9][0-9][0-9][0-9]:" | sed -r "s|([a-z]+).+|\1|g") --shell /bin/bash "$1"
+    exit $?
+  fi
 }
