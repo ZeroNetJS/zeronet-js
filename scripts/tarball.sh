37c1f133bafb1668d6eb47117c2a29ea1e2459f0
mkg20001 <mkg20001@gmail.com>
2017-07-23 09:55:39 +0200

    Fix snap

--- scripts/tarball.sh
+++ scripts/tarball.sh
@@ -15,7 +15,7 @@ if ([ "$0" = "$BASH_SOURCE" ] || ! [ -n "$BASH_SOURCE" ]);
 then
 
 ex_re
-tar cvfz zeronet.tar.gz $ex . | sed "s|^|zeronet: |g"
+tar cvfz zeronet.tar.gz $ex --mode="777" . | sed "s|^|zeronet: |g"
 ex_re
 
 for file in $(dir -w 1 | grep "^zeronet-"); do
