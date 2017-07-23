1eb34c8c1855d8ac8a948ded401ac5e787f10fbf
mkg20001 <mkg20001@gmail.com>
2017-07-23 09:56:44 +0200

    Fix scripts

--- scripts/global.sh
+++ scripts/global.sh
@@ -17,6 +17,6 @@ ver=$(echo $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g" |
 cd .pkg
 for f in package*; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../../\2.tar.gz"|g' -i $f; done
 ex_re
-tar cvfz ../znjs.tar.gz $ex . | sed "s|^|zeronet-js: |g"
+tar cvfz ../znjs.tar.gz $ex --mode="777" . | sed "s|^|zeronet-js: |g"
 mv ../znjs.tar.gz .
 npm i ./znjs.tar.gz --production -g
