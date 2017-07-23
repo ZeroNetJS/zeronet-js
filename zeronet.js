dad711e9cf88d4363917cab4c89792bbeab079f2
mkg20001 <mkg20001@gmail.com>
2017-07-23 19:05:58 +0200

    Put storage dir into it's own file

--- zeronet.js
+++ zeronet.js
@@ -7,7 +7,6 @@ let dwait = require("./lib/hacky-logs.js")
 
 const fs = require("fs")
 const path = require("path")
-const os = require("os")
 const mkdirp = require("mkdirp")
 
 const MergeRecursive = require("merge-recursive")
@@ -17,38 +16,7 @@ const FS = require("zeronet-storage-fs")
 
 const Common = require("zeronet-common")
 
-function getStorageDir() {
-  if (process.env.RUNINCWD) return path.join(process.cwd(), ".zeronet")
-  let isroot
-  switch (true) {
-  case !!process.platform.match(/^linux/):
-    isroot = !process.getuid()
-    switch (true) {
-    case !!process.env.SNAP: //snap aka ubuntu core
-      if (isroot)
-        return process.env.SNAP_COMMON
-      else
-        return process.env.SNAP_USER_COMMON
-      break;
-    default:
-      if (isroot)
-        return "/var/lib/zeronet"
-      else
-        return path.join(os.homedir(), ".zeronet")
-    }
-    break;
-  case !!process.platform.match(/^win/): //windows
-    return path.join(process.env.APPDATA, "ZeroNet")
-    break;
-  case !!process.platform.match(/^darwin/): //mac
-    return path.join(os.homedir(), "Library", "Preferences", "ZeroNet")
-    break;
-  default:
-    throw new Error("Unsupported platform " + process.platform + "! Please report this!")
-  }
-}
-
-let dir = getStorageDir()
+let dir = require("./lib/storage-dir")()
 
 mkdirp.sync(dir)
 mkdirp.sync(path.join(dir, "logs"))
