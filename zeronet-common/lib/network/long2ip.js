f08b31b5930bd550b81d3e5b3133f7e88f412021
mkg20001 <mkg20001@gmail.com>
2017-07-25 17:02:04 +0200

    Fix long2ip

--- zeronet-common/lib/network/long2ip.js
+++ zeronet-common/lib/network/long2ip.js
@@ -2,5 +2,5 @@ module.exports = function long2ip (ip) {
   if (!isFinite(ip)) {
     return false
   }
-  return [ip >>> 24, ip >>> 16 & 0xFF, ip >>> 8 & 0xFF, ip & 0xFF].join('.')
+  return [ip >>> 24 & 0xFF, ip >>> 16 & 0xFF, ip >>> 8 & 0xFF, ip & 0xFF].join('.')
 }
