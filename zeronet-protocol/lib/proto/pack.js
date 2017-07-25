9f4e00fc78261a78800c5f5b6e747a38c2c74304
kustomzone <github@kustomzone.com>
2017-07-26 01:23:37 +1200

    replace inet with functions

--- zeronet-protocol/lib/proto/pack.js
+++ zeronet-protocol/lib/proto/pack.js
@@ -5,14 +5,40 @@ const assert = require("assert")
 module.exports = function PeersPacker() {
   const self = this
   const struct = require("bufferpack")
-  const inet = require("inet")
+
+  // const inet = require("inet")
 
   self.v4 = {
-    pack: (ip, port) => inet.aton(ip) + struct.pack("H", port),
+    pack: (ip, port) => inet_aton(ip) + struct.pack("H", port),
     unpack: pack => {
       //assert(pack.length == 6, "Invalid ipv4 length")
-      return inet.ntoa(pack.split("").slice(0, 4)) + ":" + struct.unpack("H", pack, 4)
+      return inet_ntoa(pack.split("").slice(0, 4)) + ":" + struct.unpack("H", pack, 4)
     }
+  }
+  
+  // ip4 example: 192.168.2.1
+  function inet_aton(ip){
+    // split into octets
+    var a = ip.split('.');
+    var buffer = new ArrayBuffer(4);
+    var dv = new DataView(buffer);
+    for(var i = 0; i < 4; i++){
+        dv.setUint8(i, a[i]);
+    }
+    return(dv.getUint32(0));
+  }
+
+  // num example: 3232236033
+  function inet_ntoa(num){
+    var nbuffer = new ArrayBuffer(4);
+    var ndv = new DataView(nbuffer);
+    ndv.setUint32(0, num);
+
+    var a = new Array();
+    for(var i = 0; i < 4; i++){
+        a[i] = ndv.getUint8(i);
+    }
+    return a.join('.');
   }
 
   /*
