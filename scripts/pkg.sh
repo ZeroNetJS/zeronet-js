1e880c42065f63e2d5644ed43d633b512e583382
mkg20001 <mkg20001@gmail.com>
2017-07-29 11:16:30 +0200

    Use my own pkg with zeit fallback

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -12,9 +12,13 @@ if node -v | grep "^v8" > /dev/null; then
   for f in package*; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../\2.tar.gz"|g' -i $f; done
   npm i --production
   v=$(node -v | sed "s|v||g")
-  ../node_modules/.bin/pkg -t node$v-linux-x64,node$v-macos-x64,node$v-win-x64 .
-  RUNINCWD=1 TESTOK=1 ./zeronet-linux
-
+  targets="node$v-linux-x64,node$v-macos-x64,node$v-win-x64"
+  for t in $(echo "$targets" | sed "s|,| |g"); do
+    tt=$(echo "$t" | sed "s|-| |g")
+    ../node_modules/.bin/pkg-fetch $tt || PKG_REPO=zeit/pkg-fetch ../node_modules/.bin/pkg-fetch $tt
+  done
+  ../node_modules/.bin/pkg -t $targets .
+  RUNINCWD=1 TESTOK=1 ./zeronet-linux && rm -rf .zeronet
 else
   echo "Please use node v8+ to build"
 fi
