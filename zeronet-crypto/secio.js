a566e414bf67aacc5b69f189f4d6b34b0dabd0be
mkg20001 <mkg20001@gmail.com>
2017-08-07 12:16:20 +0200

    Work on secio

--- zeronet-crypto/secio.js
+++ zeronet-crypto/secio.js
@@ -4,6 +4,6 @@ const libp2p_secio = require("libp2p-secio")
 
 module.exports = function SecioCrypto(protocol, zeronet) {
   protocol.crypto.add("secio", (conn, conf, cb) => {
-    libp2p_secio.encrypt(zeronet.config.id, zeronet.config.id.privKey, conn, cb)
+    libp2p_secio.encrypt(zeronet.swarm.peerInfo.id, zeronet.swarm.peerInfo.id.privKey, conn, cb)
   })
 }
