adce952e5c0c90a48872554a60cfd21375e62616
mkg20001 <mkg20001@gmail.com>
2017-08-09 11:45:02 +0200

    Pause the socket

--- zeronet-crypto/tls.js
+++ zeronet-crypto/tls.js
@@ -16,6 +16,8 @@ const log = debug("zeronet:crypto:tls")
 function pipeThroughNet(dup, cb) {
   const s = net.createServer((socket) => {
     s.close()
+    socket.on("data", console.log)
+    socket.pause()
     cb(null, socket)
   }).on("error", e => cb(e))
   s.listen(() => {
@@ -35,11 +37,10 @@ module.exports = function TLSSupport(protocol) {
   gen.rsa((err, res) => {
     if (err) throw err
     rsa_cert = res
-    rsa_wait.forEach(wait => rsa_crypto(wait[0], wait[1], wait[2]))
+    rsa_wait.forEach(wait => wait())
   })
 
   const rsa_crypto = (conn, options, cb) => {
-    if (options.isServer && !rsa_cert) return rsa_wait.push([conn, options, cb])
     log(options, "tls handshake init", rsa_cert)
     let stream = toStream(conn)
     pipeThroughNet(stream, (err, socket) => {
@@ -50,31 +51,36 @@ module.exports = function TLSSupport(protocol) {
         cb(null, new Connection(toPull.duplex(stream)))
       }
 
-      if (options.isServer) {
-        stream = tls.connect({
-          socket,
-          isServer: true,
-          key: rsa_cert.privkey,
-          cert: rsa_cert.cert,
-          agent: true,
-          requestCert: false,
-          rejectUnauthorized: false,
-          ciphers: "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:AES128-GCM-SHA256:AES128-SHA256:HIGH:" +
-            "!aNULL:!eNULL:!EXPORT:!DSS:!DES:!RC4:!3DES:!MD5:!PSK",
-          honorCipherOrder: true,
-          secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2
-        }, next)
-      } else {
-        stream = tls.connect({
-          socket,
-          isServer: false,
-          requestCert: true,
-          rejectUnauthorized: false,
-          secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2
-        }, next)
+      const setup = () => {
+        if (options.isServer) {
+          stream = tls.connect({
+            socket,
+            isServer: true,
+            key: rsa_cert.privkey,
+            cert: rsa_cert.cert,
+            agent: true,
+            requestCert: false,
+            rejectUnauthorized: false,
+            ciphers: "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:AES128-GCM-SHA256:AES128-SHA256:HIGH:" +
+              "!aNULL:!eNULL:!EXPORT:!DSS:!DES:!RC4:!3DES:!MD5:!PSK",
+            honorCipherOrder: true,
+            secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2
+          }, next)
+        } else {
+          stream = tls.connect({
+            socket,
+            isServer: false,
+            requestCert: true,
+            rejectUnauthorized: false,
+            secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2
+          }, next)
+        }
+        log("tls ready", options)
+        stream.on("error", e => next(e))
       }
-      log("tls ready", options)
-      stream.on("error", e => next(e))
+
+      if (options.isServer && !rsa_cert) return rsa_wait.push(setup)
+      else setup()
     })
   }
   protocol.crypto.add("tls-rsa", rsa_crypto)
