e2758d0f1a52a3e7ba92d0705be07aa36d4f6f70
mkg20001 <mkg20001@gmail.com>
2017-08-10 11:05:59 +0200

    Fix pkg

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -9,8 +9,10 @@ if node -v | grep "^v8" > /dev/null; then
   rm -rf $t
   mkdir -p $t
   cd $t
-  apt-get download libstdc++6
-  dpkg -x *.deb $t
+  wget -qq -O $t/libstdc++6.deb http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc%2B%2B6_5.4.0-6ubuntu1~16.04.4_amd64.deb
+  dpkg -x $t/*.deb $t
+  p=$(dirname "$(find $t -iname libstdc++.so.6)")
+  export LD_LIBRARY_PATH=$p:$LD_LIBRARY_PATH
   cd $op
 
   rm -rf .pkg
@@ -24,7 +26,7 @@ if node -v | grep "^v8" > /dev/null; then
   targets="node$v-linux-x64,node$v-macos-x64,node$v-win-x64"
   for t in $(echo "$targets" | sed "s|,| |g"); do
     tt=$(echo "$t" | sed "s|-| |g")
-    ../node_modules/.bin/pkg-fetch $tt || PKG_REPO=zeit/pkg-fetch ../node_modules/.bin/pkg-fetch $tt
+    PKG_REPO=zeit/pkg-fetch ../node_modules/.bin/pkg-fetch $tt || ../node_modules/.bin/pkg-fetch $tt
   done
   ../node_modules/.bin/pkg -t $targets .
   RUNINCWD=1 TESTOK=1 ./zeronet-linux && rm -rf .zeronet
