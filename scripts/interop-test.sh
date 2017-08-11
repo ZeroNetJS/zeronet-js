20423607fc03501f950e1b2bf9ad4628d5eda83d
mkg20001 <mkg20001@gmail.com>
2017-08-11 22:19:47 +0200

    Use my own fixed image

--- scripts/interop-test.sh
+++ scripts/interop-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Starting zeronet python in docker container..."
-container=$(docker run -d -p 13344:15441 -p 127.0.0.1:44110:43110 nofish/zeronet) || (ex=$? && echo "Docker run failed with code $ex" && exit $ex)
+[ ! -z "$DEBUG" ] && znargs="--debug"
+container=$(docker run -d -p 13344:15441 -p 127.0.0.1:44110:43110 mkg20001/zeronet-docker --ui_ip 0.0.0.0 $znargs) ; ex=$? ; [ $ex -ne 0 ] && echo "Docker run failed with code $ex" && exit $ex
 
 docker logs -f $container &
 
