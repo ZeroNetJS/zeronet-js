bc6e0aec22081befe0cf5a2b738b40d91720c752
mkg20001 <mkg20001@gmail.com>
2017-08-24 14:55:25 +0200

    Change test name

--- test/protocol/pack.js
+++ test/protocol/pack.js
@@ -8,7 +8,7 @@ const map = {
   "\x7f\x00\x00\x01\xb0\x9d": "127.0.0.1:40368",
   "\x05\x01\xad\xf0\xd8\x0f": "5.1.173.240:4056"
 }
-describe("protocol", () => {
+describe("pack", () => {
   Object.keys(map).forEach(packed => {
     it("should unpack " + JSON.stringify(packed) + " as " + map[packed], () => assert.equal(pack.v4.unpack(packed), map[packed]))
     it("should pack " + map[packed] + " as " + JSON.stringify(packed), () => assert.equal(pack.v4.pack(map[packed]), packed))
