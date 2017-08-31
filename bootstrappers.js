68e216be730be669d55f17ae867cf8dbe8555a38
mkg20001 <mkg20001@gmail.com>
2017-08-31 11:08:11 +0200

    Add https signaling servers

--- bootstrappers.js
+++ bootstrappers.js
@@ -1,8 +1,10 @@
 //Offical ZeroNet Bootstrapping servers
 module.exports = {
   wstar: [
-    "/libp2p-webrtc-star/ip4/148.251.206.162/tcp/9090/ws/",
-    "/libp2p-webrtc-star/ip4/136.243.31.32/tcp/4278/ws/",
+    //"/libp2p-webrtc-star/ip4/148.251.206.162/tcp/9090/ws/",
+    "/libp2p-webrtc-star/dns4/ws-star-signal-1.servep2p.com/wss/",
+    //"/libp2p-webrtc-star/ip4/136.243.31.32/tcp/4278/ws/",
+    "/libp2p-webrtc-star/dns4/ws-star-signal-2.servep2p.com/wss/",
     "/libp2p-webrtc-star/dns4/localhost/ws/"
   ],
   libp2p: [
