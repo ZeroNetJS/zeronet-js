65ef46a35a389074c6d39e2e13a759a61dc6c73d
mkg20001 <mkg20001@gmail.com>
2017-09-01 21:24:18 +0200

    Add bootstrappers file to pkg bundle

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -17,7 +17,7 @@ if node -v | grep "^v8" > /dev/null; then
 
   rm -rf .pkg
   mkdir .pkg
-  for f in package* zeronet.js lib; do cp -r $f .pkg; done
+  for f in package* bootstrappers.js zeronet.js lib; do cp -r $f .pkg; done
   #ver=$(echo $(cat package.json | grep "version" | sed "s|\"||g" | sed "s|  ||g" | grep " .*" -o) | sed "s|,||g")
   cd .pkg
   for f in package*; do sed -r 's|"([a-z-]+)": "file:(.*)"|"\1": "file:../\2.tar.gz"|g' -i $f; done
