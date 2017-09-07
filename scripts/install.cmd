2c4e0be3ce879a2a05a0d70adc8bbecbc4b27f2b
mkg20001 <mkg20001@gmail.com>
2017-09-07 19:43:48 +0200

    Use symlink for windows too

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-npm i&& for /D %%i in (zeronet-*) do (cd "%%i"&& npm i&& cd ..)
+npm i&& for /D %%i in (zeronet-*) do (cd "%%i"&& mklink /D node_modules ..\node_modules&& cd ..)
 
 goto :eof
