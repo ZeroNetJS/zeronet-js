2b12b93d69e707d41d220494b7f835a05baa253c
mkg20001 <mkg20001@gmail.com>
2017-09-09 12:01:17 +0200

    Brand new beta bootstrapper 3

--- bootstrappers.js
+++ bootstrappers.js
@@ -1,10 +1,9 @@
 //Offical ZeroNet Bootstrapping servers
 module.exports = {
   wstar: [
-    //"/libp2p-webrtc-star/ip4/148.251.206.162/tcp/9090/ws/",
     "/dns4/ws-star-signal-1.servep2p.com/wss/p2p-websocket-star/",
-    //"/libp2p-webrtc-star/ip4/136.243.31.32/tcp/4278/ws/",
-    "/dns4/ws-star-signal-2.servep2p.com/wss/p2p-websocket-star/",
+    "/dns/ws-star-signal-2.servep2p.com/wss/p2p-websocket-star/",
+    "/dns4/ws-star-signal-3.servep2p.com/wss/p2p-websocket-star/",
     "/dns4/localhost/ws/p2p-websocket-star/"
   ],
   libp2p: [
