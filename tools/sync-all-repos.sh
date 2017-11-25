26ace5fb95dcdecde897fbb1c294d31708c60747
mkg20001 <mkg20001@gmail.com>
2017-11-25 12:55:03 +0100

    Fix sync

--- tools/sync-all-repos.sh
+++ tools/sync-all-repos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirname $(basename $0))
+cd $(dirname $(readlink -f $0))
 
 repos=$(cat repos.txt)
 files=$(cat sync-files.txt)
@@ -10,6 +10,8 @@ mkdir -p tmp
 cd tmp
 
 for r in $repos; do
+  echo "[SYNC] $r"
+
   [ ! -e $r ] && git clone git@github.com:ZeroNetJS/$r $r -b master
 
   cd $r
@@ -18,11 +20,12 @@ for r in $repos; do
   git pull origin master
 
   for f in $files; do
-    cp ../../$f .
+    cp ../../../$f .
     git add $f
   done
 
   git commit -m "misc: Sync repo files"
+  git push
 
   cd ..
 done
