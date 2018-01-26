84b0769875b137a75c19a2f02a39428e3196bf95
mkg20001 <mkg20001@gmail.com>
2018-01-26 20:17:16 +0100

    Update trackers list

--- src/peers.js
+++ src/peers.js
@@ -16,15 +16,16 @@ module.exports = {
   relay: [
     '/dnsaddr/znjs-relay.servep2p.com/tcp/443/wss/p2p-znjs-relay/ipfs/QmVdTP7ve4AhrYMzQALEtbXojAYsZAMXTTwyJq8w46CosQ'
   ],
-  trackers: [
-    // "zero://boot3rdez4rzn36x.onion:15441", // TODO: need onion transport
-    // "zero://boot.zeronet.io#f36ca555bee6ba216b14d10f38c16f7769ff064e0e37d887603548cc2e64191d:15441", // TODO: need onion trackers
+  trackers: [ // taken from here https://github.com/HelloZeroNet/ZeroNet/blob/master/src/Config.py#L35-L44
+    // "zero://boot3rdez4rzn36x.onion:15441", TODO: tor
+    // "zero://zero.booth.moe#f36ca555bee6ba216b14d10f38c16f7769ff064e0e37d887603548cc2e64191d:15441", TODO: figure out what the hashtag means
+    // "zero://216.189.144.82:15441", TODO: https://github.com/ZeroNetJS/zeronet-tracker/issues/4
     'udp://tracker.coppersurfer.tk:6969',
     'udp://tracker.leechers-paradise.org:6969',
     'udp://9.rarbg.com:2710',
     'http://tracker.opentrackr.org:1337/announce',
     'http://explodie.org:6969/announce',
-    'http://tracker1.wasabii.com.tw:6969/announce'
+    'http://retracker.spark-rostov.ru:80/announce'
     // "http://localhost:25534/announce" // DEV ONLY
   ]
 }
