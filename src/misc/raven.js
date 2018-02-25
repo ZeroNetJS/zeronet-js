93031ab6f62f19e2cd759d91e478f2ce7bf510c9
mkg20001 <mkg20001@gmail.com>
2018-02-25 17:57:42 +0100

    Raven browser fix

--- src/misc/raven.js
+++ src/misc/raven.js
@@ -5,17 +5,16 @@ const log = debug('zeronet:sentry')
 
 if (process.env.DISABLE_SENTRY || global.DISABLE_SENTRY) return log('sentry disabled by user')
 
-const setup = () => {
-  const Raven = require('raven')
+const Raven = require('raven') // will/should be replaced by raven-js if in browser
+
+const setup = () =>
   Raven.config('https://f2a666530dc342d3994e3a91aa7ea903:44e81d0ef80f43c498d52cd7f667a900@sentry.zion.host/5').install()
-}
 
 if (process.env.SNAP && !process.env.SNAP_REVISION.startsWith('x')) { // detect snap production
   log('setting up sentry for snap')
   setup()
 } else if (typeof window === 'object' && window.location.host == 'zeronetjs.github.io') { // detect web production
   log('setting up sentry for web')
-  const Raven = require('raven-js') // raven-js is for web
   Raven.config('https://f2a666530dc342d3994e3a91aa7ea903@sentry.zion.host/5').install()
 } else if (process.argv[1].matches(/^\/usr.*\/bin\/zeronet$/) && __dirname.matches(/^\/usr\/.*\/node_modules\/.+$/)) {
   log('setting up sentry for nodejs-cli')
